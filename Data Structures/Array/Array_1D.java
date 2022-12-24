4580bdd11036be1c671ad582bf9d0a06092b2b3e
Ashi Malaiya <91681952+Ashi777@users.noreply.github.com>
2022-12-25 00:10:35 +0530

    Update Array_1D.java

--- Data Structures/Array/Array_1D.java
+++ Data Structures/Array/Array_1D.java
@@ -10,14 +10,18 @@ public class Array_1D
 		//To display all the elements
 		System.out.println("The elements of the array are :");
 		for(int i=0;i<arr.length;i++)
+		{
 			System.out.print(arr[i]+"  ");
+		}
 		
 		//To display odd elements
 		System.out.println("\n\nThe odd elements are :");
 		for(int j=0;j<arr.length;j++)
 		{
 			if(arr[j]%2!=0)
+			{
 				System.out.print(arr[j]+"  ");
+			}
 		}
 		
 		//To display even elements
@@ -25,14 +29,18 @@ public class Array_1D
 		for(int k=0;k<arr.length;k++)
 		{
 			if(arr[k]%2==0)
+			{
 				System.out.print(arr[k]+"  ");
+			}
 		}
 		
 		//To display sum of all elements
 		System.out.println("\n\nThe sum of all elements is :");
 		int sum=0;
 		for(int a=0;a<arr.length;a++)
+		{
 			sum=sum+arr[a];
+		}
 		System.out.print(sum);
 		
 		//To display sum of odd index elements
@@ -41,7 +49,9 @@ public class Array_1D
 		for(int b=0;b<arr.length;b++)
 		{
 			if(b%2!=0)
+			{
 				sum1=sum1+arr[b];
+			}
 		}
 		System.out.print(sum1);
 		
@@ -51,7 +61,9 @@ public class Array_1D
 		for(int c=0;c<arr.length;c++)
 		{
 			if(c%2==0)
+			{
 				sum2=sum2+arr[c];
+			}
 		}
 		System.out.print(sum2);
 		
@@ -61,7 +73,9 @@ public class Array_1D
 		for(int d=0;d<arr.length;d++)
 		{
 			if(arr[d]>largest)
+			{
 				largest=arr[d];
+			}
 		}
 		System.out.print(largest);
 		
@@ -71,7 +85,9 @@ public class Array_1D
 		for(int e=0;e<arr.length;e++)
 		{
 			if(secondlargest<arr[e] && arr[e]<largest)
+			{
 				secondlargest=arr[e];
+			}
 		}
 		System.out.print(secondlargest);
 		
@@ -81,7 +97,9 @@ public class Array_1D
 		for(int p=0;p<arr.length;p++)
 		{
 			if(thirdlargest<arr[p] && arr[p]<secondlargest)
+			{
 				thirdlargest=arr[p];
+			}
 		}
 		System.out.print(thirdlargest);
 		
@@ -101,7 +119,9 @@ public class Array_1D
 		}
 		System.out.println("\n\nThe elements in ascending order are :");
 		for(int x=0;x<arr.length;x++)
+		{
 			System.out.print(arr[x]+"  ");
+		}
 		
 		//To sort the array in descending order
 		int temp1;
@@ -119,6 +139,8 @@ public class Array_1D
 		}
 		System.out.println("\n\nThe elements in descending order are :");
 		for(int y=0;y<arr.length;y++)
+		{
 			System.out.print(arr[y]+"  ");
+		}
 	}
 }
